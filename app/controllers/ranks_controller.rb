1c9c72cac1aa2a0bee8f5f628f2af47c69814942
Shikhir Aggarwal <shikhir.aggarwal@daffodilsw.com>
2019-05-28 18:27:02 +0530

    Add root home page

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,9 +1,9 @@
 class RanksController < ApplicationController
   def index
-  	player_id = params[:id].to_i
-  	player = (player_id == 0) ? nil : Player.find(player_id)
-  	if player.nil?
-  	  @rankings = Player.get_rankings
+    player_id = params[:id].to_i
+    player = (player_id == 0) ? nil : Player.find(player_id)
+    if player.nil?
+      @rankings = Player.get_rankings
       @rank = 0
       @page = 1
       @points = 0
@@ -14,7 +14,7 @@ class RanksController < ApplicationController
       @rank = data["rank"]
       @page = data["page"]
       @points = player.points
-  	end
+    end
     response = {"rankings" => @rankings, "rank" => @rank, "page" => @page, "points" => @points}
     respond_to do |format|
       format.html # show.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   get 'terms/index'
   get 'terms/fixtures'
   get 'terms/teams'
